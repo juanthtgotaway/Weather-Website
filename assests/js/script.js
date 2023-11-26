175573a9d4993259c86a9ca10007f50398e8ca44
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-26 13:49:22 -0700

    code works pulling API info, need to add to show weather

---
+++ assests/js/script.js
@@ -0,0 +1,34 @@
+var cityFormEl = document.querySelector('#user-city');
+var currentWeatherEl = document.querySelector('#currentweather');
+var forecastCardsEl = document.querySelector('#forecastcards');
+
+var citySubmitHandler = function (event) {
+  event.preventDefault();
+  var cityName = document.querySelector('#city').value.trim();
+
+  if (cityName) {
+    getWeatherData(cityName);
+    currentWeatherEl.textContent = '';
+    forecastCardsEl.textContent = '';
+  }
+};
+
+var getWeatherData  = function (cityName) {
+    var apiURl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=fad8ad47e93c490afb3a455a487c8ce0";
+
+    fetch(apiURl)
+     .then(function(response){
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                displayWeather(data, user);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+     });
+}
+
+
+cityFormEl.addEventListener('submit', citySubmitHandler);
